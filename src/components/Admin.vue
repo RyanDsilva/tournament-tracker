3d164bca78ed675c3fd7d2653843b0ae3606222b
Ryan <ryan.dsilva.98@gmail.com>
2018-08-17 20:19:33 +0530

    Added Fetch Scores functionality

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,10 +1,21 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li v-for="score in scores" v-bind:key="score.id">
+        {{score.prop}} - {{score.pscore}} || {{score.opp}} - {{score.oscore}}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
   name: 'Admin',
+  computed: {
+    ...mapState(['currentUser', 'scores']),
+  },
 };
 </script>
 
--- src/store.js
+++ src/store.js
@@ -5,10 +5,32 @@ const fb = require('./firebase');
 
 Vue.use(Vuex);
 
+fb.auth.onAuthStateChanged(user => {
+  if (user) {
+    store.commit('setCurrentUser', user);
+    // realtime updates from our posts collection
+    fb.scores
+      .orderBy('dnum', 'desc')
+      // eslint-disable-next-line
+      .onSnapshot(querySnapshot => {
+        let scoresList = [];
+
+        querySnapshot.forEach(doc => {
+          let score = doc.data();
+          score.id = doc.id;
+          scoresList.push(score);
+        });
+
+        store.commit('setScores', scoresList);
+      });
+  }
+});
+
 // eslint-disable-next-line
 export const store = new Vuex.Store({
   state: {
     currentUser: null,
+    scores: [],
   },
   actions: {
     clearData({ commit }) {
@@ -18,6 +40,9 @@ export const store = new Vuex.Store({
   mutations: {
     setCurrentUser(state, val) {
       state.currentUser = val;
+    },
+    setScores(state, val) {
+      state.scores = val;
     },
   },
 });

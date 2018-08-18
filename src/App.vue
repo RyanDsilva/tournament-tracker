19826bc787b4c8b5547f18ef479acee183552c4e
Ryan <ryan.dsilva.98@gmail.com>
2018-08-18 11:55:23 +0530

    Changed Font

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <v-app>
+  <v-app id="app">
     <v-navigation-drawer app temporary absolute v-model="drawer">
       <v-toolbar flat>
         <v-list>
           <v-list-tile>
             <v-list-tile-title class="title">
-              CRMD
+              CRMD 2018
             </v-list-tile-title>
           </v-list-tile>
         </v-list>
@@ -29,9 +29,12 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar app color="indigo" dark>
+    <v-toolbar app color="deep-purple darken-3" dark>
       <v-toolbar-side-icon @click.stop="drawer=!drawer"></v-toolbar-side-icon>
-      <v-toolbar-title class="white--text">CRMD</v-toolbar-title>
+      <v-avatar>
+        <img src="../static/android-chrome-192x192.png" alt="LOGO">
+      </v-avatar>
+      <v-toolbar-title class="white--text">CRMD 2018</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn to="home" flat>Home</v-btn>
@@ -46,7 +49,7 @@
         <router-view></router-view>
       </v-container>
     </v-content>
-    <v-footer color="indigo" dark>
+    <v-footer color="deep-purple darken-3" dark>
       <v-spacer></v-spacer>
       &copy; 2018 Ryan Dsilva
       <v-spacer></v-spacer>
@@ -86,3 +89,12 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.v-toolbar__title:not(:first-child) {
+  margin-left: 0;
+}
+#app {
+  font-family: 'Open Sans', sans-serif;
+}
+</style>

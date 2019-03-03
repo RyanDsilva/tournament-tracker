18f91adef5f3fc5dae87d390e79d5978ed38c10c
Ryan Dsilva <ryan.dsilva.98@gmail.com>
2019-03-03 22:17:51 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,5 @@
 import firebase from 'firebase';
 
-const config = {
-  apiKey: 'AIzaSyBG5KqYufAFVPwG9dB8Jknaqu7OBC1QSgk',
-  authDomain: 'crmd-18-stuco.firebaseapp.com',
-  databaseURL: 'https://crmd-18-stuco.firebaseio.com',
-  projectId: 'crmd-18-stuco',
-  storageBucket: 'crmd-18-stuco.appspot.com',
-  messagingSenderId: '984258467854',
-};
 firebase.initializeApp(config);
 
 const db = firebase.firestore();
